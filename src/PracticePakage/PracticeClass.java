7549ae1b4ed7abf2289022b7b260c9c75629e057
Ibrahim Farzam <ibrahimfarzam500@gamil.com>
2024-05-06 18:51:00 -0400

    just wat to make sure everything is ready for jinking

---
+++ src/PracticePakage/PracticeClass.java
@@ -0,0 +1,11 @@
+package PracticePakage;
+
+public class PracticeClass {
+	public static void main(String[] args) {
+		
+		System.out.println("This is a practice for jinkin");
+		
+		System.out.println("Making some changies");
+	}
+
+}
